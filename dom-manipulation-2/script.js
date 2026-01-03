d72dea3f77d971d3e64225648db7b008fe25a531
vishwas-28cpu <vishwas.work28@gmail.com>
2026-01-03 22:03:23 +0530

    first commit

---
+++ dom-manipulation-2/script.js
@@ -0,0 +1,49 @@
+var arr = [
+   {
+    team: 'mi',
+    wins:  5,
+    src: "images/mi.png"
+
+   } ,
+    {
+    team: 'rcb',
+    wins: 1,
+     src: "images/rcb.png"
+    } ,
+    {
+    team: 'csk',
+    wins: 5,
+      src: "images/csk.png"
+    
+   } ,
+    {
+    team: 'kkr',
+    wins: 3,
+    src: "images/kkr.png"
+   } ,
+    {
+    team: 'srh',
+    wins: 2,
+    src: "images/srh.png"
+
+   } , 
+   {
+    team: 'rr',
+    wins: 1,
+    src:  "images/rr.png"
+
+   } ,
+    {
+    team: "gt",
+    wins:  1,
+    src: "images/gt.png"
+
+   } ,
+    {
+    team: 'dc',
+    wins: 1,
+    src: "images/dc.png"
+
+   } ,
+
+]
