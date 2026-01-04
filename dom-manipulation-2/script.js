9f9999c803856bca849a7dd082983e6d8702c3d1
vishwas-28cpu <vishwas.work28@gmail.com>
2026-01-04 10:45:44 +0530

    final commit

--- dom-manipulation-2/script.js
+++ dom-manipulation-2/script.js
@@ -1,4 +1,14 @@
-var arr = [
+var h1 = document.querySelector("h1");
+var Card = document.querySelector("#card");
+var teamname = document.querySelector("#teamname");
+var trophy = document.querySelector("#trophy");
+var trophywins = document.querySelector("#trophywins");
+var img = document.querySelector("img");
+var btn = document.querySelector("button");
+
+
+
+var teams = [
    {
     team: 'mi',
     wins:  5,
@@ -47,3 +57,9 @@ var arr = [
    } ,
 
 ]
+btn.addEventListener("click", function(){
+   var t = Math.floor(Math.random() * teams.length);
+   teamname.textContent = teams[t].team;
+   trophywins.textContent = "trophy wins: " + teams[t].wins;
+   img.src = teams[t].src;
+});
